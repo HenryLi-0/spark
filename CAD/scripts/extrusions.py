cfd1ec81fb3f8b993e8cfebea47464711d64a24e
HenryLi-0 <bxsci8266@gmail.com>
2026-01-26 01:32:08 -0500

    01252026 - doing some BOM optimization updates!

--- CAD/scripts/extrusions.py
+++ CAD/scripts/extrusions.py
@@ -33,21 +33,21 @@ def calculate(length:int, count:int, target:list[int, int]):
 
 
 
-# e_count = 8 # number of extrusions
-# e_length = 100 # keep units consistent, cm for now
+# e_count = 4 # number of extrusions
+# e_length = 122 # keep units consistent, cm for now
 
-# e_Z_HEIGHT = 35
+# Z_HEIGHT = 18
 # e_target = [ # required lengths, order by LENGTH, COUNT
-#     [30, 3],
-#     [34, 2],
+#     [26, 3],
+#     [26, 2],
 #     [6, 2],
-#     [e_Z_HEIGHT, 4],
-#     [30, 2],
-#     [34, 2],
+#     [Z_HEIGHT, 4],
+#     [26, 2],
+#     [26, 2],
 #     [6, 2],
-#     [30, 2],
-#     [30, 2],
-#     [42, 2]
+#     [26, 1],
+#     [26, 2],
+#     [34, 2]
 # ]
 
 # results = calculate(e_length, e_count, e_target)
@@ -55,15 +55,15 @@ def calculate(length:int, count:int, target:list[int, int]):
 #     for i, length in enumerate(results[1], 1):
 #         print("extrusion {} (leftover {}): {}".format(i, length[0], length[1]))
 
-r_count = 4
+r_count = 2
 r_length = 100
 
 r_Z_HEIGHT = 35
 r_target = [
-    [46, 4],
-    [32.6, 2],
-    [13.6, 2],
-    [r_Z_HEIGHT, 2]
+    [38, 2],
+    [25.6, 2],
+    [15.1, 2],
+    [18, 2]
 ]
 
 results = calculate(r_length, r_count, r_target)
